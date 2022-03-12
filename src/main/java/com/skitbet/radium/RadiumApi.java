2158e350b4e44328e3189c67fa08f2974b28e872
Skitbet <54096334+Skitbet@users.noreply.github.com>
2022-03-11 22:55:33 -0500

    Comments

--- src/main/java/com/skitbet/radium/RadiumApi.java
+++ src/main/java/com/skitbet/radium/RadiumApi.java
@@ -8,10 +8,12 @@ import lombok.Getter;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.reflections.Reflections;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.security.auth.login.LoginException;
+import java.util.Set;
 
 public class RadiumApi {
 
@@ -31,23 +33,40 @@ public class RadiumApi {
         bot.addEventListener(radiumMenuManager);
     }
 
-
+    /**
+     * Add a command to the bot, you need to rejoin the bot if you are testing, if you don't it can take up to 24 hours to update.
+     * @param command The command to add
+     */
     public void addCommand(ICommand command) {
         slashCommandManager.addCommand(command);
     }
 
+    /**
+     * Register all commands that have been added
+     */
     public void registerCommands() {
         slashCommandManager.registerAllCommands();
     }
 
+    /**
+     * Add a menu to the bot and register it.
+     * @param menu The menu to add
+     */
 
     public void addMenu(IMenu menu) {
         radiumMenuManager.addMenu(menu);
     }
 
+    /**
+     * Get a menu that has been registered on the bot.
+     * @param name The name of the menu to get
+     * @return The menu you are looking for, will return null if it does not exist.
+     */
+
     public IMenu getMenu(String name) {
         return radiumMenuManager.getMenu(name);
     }
+
 
 
 
